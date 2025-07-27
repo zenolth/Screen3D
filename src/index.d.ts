b5406235134617c7526770971a1d52e3a537521c
zenolth <madarik.sim@gmail.com>
2025-07-27 21:46:16 +0200

    Replaced the rest of `any` types with their correct types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,20 +1,20 @@
-interface componentGen {
+interface component3D {
     enabled : boolean;
 			
     component2D : GuiObject | undefined;
     surfaceGui : SurfaceGui | undefined;
     
     parent2D : GuiObject | undefined;
-    screen3D : screenGen,
-    parent3D : componentGen | undefined,
+    screen3D : screen3D,
+    parent3D : component3D | undefined,
 
     offset : CFrame,
     viewportSize : Vector2,
 
     conn : RBXScriptConnection | undefined;
 
-    Enable(): componentGen;
-	Disable(): componentGen;
+    Enable(): component3D;
+	Disable(): component3D;
 	RecomputeParent(): void;
 	
 	GetStudsScreenSize(viewportSize : Vector2): Vector3;
@@ -23,26 +23,26 @@ interface componentGen {
 	GetViewportSize(): CFrame;
 }
 
-interface componentConstructor {
-    new(Component2D: GuiObject, Screen3D: screenGen): componentGen;
+interface componentGen {
+    new(Component2D: GuiObject, Screen3D: screen3D): component3D;
 }
 
-interface screenGen {
-    partIndex : Map<GuiObject,any>,
+interface screen3D {
+    partIndex : Map<GuiObject,component3D>,
     rootGui : ScreenGui,
     displayDistance : number,
     rootOffset : CFrame
 
-    GetComponent3D(Component2D: GuiObject): any;
+    GetComponent3D(Component2D: GuiObject): component3D;
     GetRealCanvasSize(): Vector2;
 	GetInsetCanvasSize(): Vector2;
 	GetIntendedCanvasSize(): Vector2;
 	GetInset(): Vector2;
 }
 
-interface screenGenConstructor {
-    new(screenGui: ScreenGui, displayDistance: number): screenGen;
+interface screenGen {
+    new(screenGui: ScreenGui, displayDistance: number): screen3D;
 }
 
-declare const screenGen: screenGenConstructor;
+declare const screenGen: screenGen;
 export = screenGen;
